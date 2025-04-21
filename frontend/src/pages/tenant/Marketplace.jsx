c20060738c1e1b1832d04dcd26b4d2ca4c207075
clifford-obure <cliffobure@gmail.com>
2025-04-21 23:36:22 +0300

    text-white

--- frontend/src/pages/tenant/Marketplace.jsx
+++ frontend/src/pages/tenant/Marketplace.jsx
@@ -157,7 +157,7 @@ const TenantMarketplace = () => {
               />
               <button
                 type="submit"
-                className="inline-flex items-center px-4 py-2 border border-transparent rounded-r-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700"
+                className="inline-flex items-center px-4 py-2 border border-transparent rounded-r-md shadow-sm text-sm font-medium  bg-primary-600 hover:bg-primary-700"
               >
                 <FaSearch />
               </button>
